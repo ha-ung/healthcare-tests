35e11c9f3aaa57ec03a67c6fdead08d4b450529f
ha-ung <ha.ung@katalon.com>
2025-09-08 14:25:46 +0700

    Updated

---
+++ Test Suites/New Dynamic Test Suite.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>New Dynamic Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>69ee3eda-239d-460f-b35e-fa9aa1864e74</testSuiteGuid>
+   <filteringText></filteringText>
+</FilteringTestSuiteEntity>
