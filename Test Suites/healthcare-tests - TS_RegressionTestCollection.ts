187bc69e5963f5ca09a9393f09ca48d3ca814479
ha-ung <ha.ung@katalon.com>
2025-10-08 15:11:38 +0700

    Updated

--- Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
+++ Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
@@ -49,22 +49,22 @@
                   <key>testCloudMobileDevice</key>
                   <value>{
   &quot;capability&quot;: {
-    &quot;availability&quot;: &quot;HIGH&quot;
+    &quot;availability&quot;: &quot;MEDIUM&quot;
   },
-  &quot;id&quot;: &quot;apple_iphone_16&quot;,
+  &quot;id&quot;: &quot;apple_ipad_pro_12.9_2020&quot;,
   &quot;isTablet&quot;: false,
   &quot;manufacturer&quot;: [
     &quot;Apple&quot;
   ],
-  &quot;mobileDeviceId&quot;: &quot;iPhone_16_ios_18&quot;,
-  &quot;name&quot;: &quot;Apple iPhone 16&quot;,
-  &quot;displayName&quot;: &quot;Apple iPhone 16&quot;,
-  &quot;osVersion&quot;: &quot;18&quot;,
+  &quot;mobileDeviceId&quot;: &quot;iPad_Pro_12_9_26_beta_real_sjc1&quot;,
+  &quot;name&quot;: &quot;Apple iPad Pro 12.9 2020&quot;,
+  &quot;displayName&quot;: &quot;Apple iPad Pro 12.9 2020&quot;,
+  &quot;osVersion&quot;: &quot;26.1&quot;,
   &quot;os&quot;: &quot;iOS&quot;,
   &quot;platformName&quot;: &quot;IOS&quot;,
   &quot;timeout&quot;: 15,
   &quot;usingTunnel&quot;: false,
-  &quot;isHighAvailableDevice&quot;: true
+  &quot;isHighAvailableDevice&quot;: false
 }</value>
                </entry>
                <entry>
