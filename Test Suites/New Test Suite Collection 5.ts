1859a15d97ec198b6d22332c580befdb2f07e966
ha-ung <ha.ung@katalon.com>
2025-10-13 10:57:03 +0700

    Updated

--- Test Suites/New Test Suite Collection 5.ts
+++ Test Suites/New Test Suite Collection 5.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>New Test Suite Collection 4</name>
+   <name>New Test Suite Collection 5</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
