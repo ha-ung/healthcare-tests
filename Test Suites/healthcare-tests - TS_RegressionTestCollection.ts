08ad9a770764d64466f6083ec9578750c7c60a12
ha-ung <ha.ung@katalon.com>
2026-01-29 17:33:21 +0700

    Updated

--- Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
+++ Test Suites/healthcare-tests - TS_RegressionTestCollection.ts
@@ -12,7 +12,7 @@
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
             <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
+            <runConfigurationId>Edge Chromium</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/healthcare-tests - TS_RegressionTest</testSuiteEntity>
